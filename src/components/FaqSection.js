0c9e4f1c1cc4011f74653bf7f7847ca29bfca745
Rilwan Yusuf <ryusufu@gmail.com>
2020-12-22 10:32:43 -0800

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start ?</h4>
+                <p className="answer">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, provident.</p>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <p className="answer">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, provident.</p>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <p className="answer">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, provident.</p>
+            </div>
+            <div className="question">
+                <h4>What Products do you offer ?</h4>
+                <p className="answer">
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, provident.</p>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
