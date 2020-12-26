30be22dfea6d6ac1a9e5b73616a1242b5442c4fe
Rilwan Yusuf <ryusufu@gmail.com>
2020-12-26 10:18:37 -0800

    9. Added Movie section in OurWork

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import AboutUs from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav'
+import ContactUs from './components/ContactUs'
+import OurWork from './components/OurWork'
+import { Switch, Route } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+
+        <Route path="/work">
+          <OurWork />
+        </Route>
+
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
